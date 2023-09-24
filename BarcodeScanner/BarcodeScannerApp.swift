3d650510e6b3dd53be7e126178c384f2cf91d106
Max Soiferman <sooiferman@gmail.com>
2023-09-24 17:37:07 -0400

    Init commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Max Soiferman on 24/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
